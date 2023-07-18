52c3f2aa35c953a868b619a8a4aa6e7df83fb66e
AnnZblt <ann.zblt@yandex.ru>
2023-07-18 18:40:01 +0300

    Add edits by HW

--- part_1/index.js
+++ part_1/index.js
@@ -1,7 +1,8 @@
 const container = document.createElement('div');
 const phraseInput = document.createElement('input');
 const phraseParagraph = document.createElement('p');
-let timer;
+const paragraphPlaceHolder = 'Тут будет фраза из инпута';
+let inputTimer;
 
 phraseInput.placeholder = 'Напишите здесь что-нибудь';
 document.body.append(container);
@@ -9,14 +10,19 @@ container.append(phraseInput, phraseParagraph);
 container.classList.add('container');
 
 
-phraseParagraph.textContent = 'Тут будет фраза из инпута';
+phraseParagraph.textContent = paragraphPlaceHolder;
 
 phraseInput.addEventListener('input', event => {
   if (event) {
-    clearInterval(timer);
+    clearTimeout(inputTimer);
   }
-  timer = setInterval(() => {
-    phraseParagraph.textContent = event.target.value;
-    console.log(phraseParagraph);
+  inputTimer = setTimeout(() => {
+    const inputText = event.target.value;
+    if (inputText.trim() === '') {
+      phraseParagraph.textContent = paragraphPlaceHolder;
+    } else {
+      phraseParagraph.textContent = event.target.value;
+      console.log(phraseParagraph);
+    }
   }, 300);
 });
